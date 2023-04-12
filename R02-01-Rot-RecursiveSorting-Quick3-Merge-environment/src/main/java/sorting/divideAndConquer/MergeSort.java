dc369f24463d0adc5c5fe864d6fd2bf7c85cea7f
rayaneBSilva <rayane.silva@ccc.ufcg.edu.br>
2023-04-12 09:48:21 -0300

    adding roteiro 2 changes

--- R02-01-Rot-RecursiveSorting-Quick3-Merge-environment/src/main/java/sorting/divideAndConquer/MergeSort.java
+++ R02-01-Rot-RecursiveSorting-Quick3-Merge-environment/src/main/java/sorting/divideAndConquer/MergeSort.java
@@ -2,6 +2,8 @@ package sorting.divideAndConquer;
 
 import sorting.AbstractSorting;
 
+import java.util.Arrays;
+
 /**
  * Merge sort is based on the divide-and-conquer paradigm. The algorithm
  * consists of recursively dividing the unsorted list in the middle, sorting
@@ -12,7 +14,57 @@ public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		   if(validation(array, leftIndex, rightIndex)){
+
+			int middle = (rightIndex + leftIndex )/2;
+			sort(array, leftIndex, middle);
+			sort(array, middle + 1, rightIndex);
+			merge(array, leftIndex, rightIndex, middle);
+
+		}
+	}
+
+	private boolean validation(T[] array, int leftIndex, int rightIndex) {
+		boolean result = true;
+		if(array == null)
+		   result = false;
+		if(array.length == 0)
+			result = false;
+		if(leftIndex < 0 || rightIndex < 0)
+			result = false;
+		if(rightIndex >= array.length)
+			result = false;
+		if(leftIndex > rightIndex)
+			result = false;
+		return result;
+	}
+
+	private void merge(T[] array, int leftIndex, int rightIndex, int middle) {
+		T[] helper = Arrays.copyOf(array, array.length);
+
+		int i = leftIndex;
+		int j = middle + 1;
+		int k = leftIndex;
+
+		while(i <= middle && j <= middle){
+			if(helper[j].compareTo(helper[i]) >  0){
+				array[k] = helper[i];
+				i++;
+			} else {
+				array[k] = helper[j];
+				j++;
+			}
+			k++;
+		}
+		while(i <= middle){
+			array[k] = helper[i];
+			i++;
+			k++;
+		}
+		while(j <= rightIndex){
+			array[k] = helper[j];
+			j++;
+			k++;
+		}
 	}
 }
